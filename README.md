c52c1ec8f8e47f1ab103145546bd3c9ec8717caf
Jendrik Poloczek <jendrik@pilzspore.de>
2012-02-12 22:27:35 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# tinybuddha-wisdom
+
+The [Tiny Buddha](http://www.tinybuddha.com) wisdoms for your shell. Thanks Lori (the author) for granting the use of her site in such nerdy way :).
+
+Add it to your shell startup configuration and everytime you login or spawn a shell you'll be greeted by the latest wisdom quote.
+
+## Example
+
+<pre>
+Tiny Buddha: “Uncertainty is the only certainty there is, and knowing how to live with insecurity is the only security.” -John Allen Paulos
+localhost :: ~ % 
+</pre>
+
+## Installation
+
+Add the following line to your e.g. .zshrc, .bashrc. It depends which shell you're actually using. It is important to use python 3 (because http.client is used).  
+
+<pre>
+python3 /home/jendrik/tinybuddha-fortune/tinybuddha-fortune/tinybuddha-fortune.py
+</pre>
+
+## License 
+
+This file is part of tinybuddha-wisdom.
+
+tinybuddha-wisdom is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+tinybuddha-wisdom is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with tinybuddha-wisdom. If not, see <http://www.gnu.org/licenses/>.
+
+
+
---
+++ tinybuddha-wisdom/tinybuddha-wisdom.py
@@ -0,0 +1,44 @@
+"""
+This file is part of tinybuddha-wisdom.
+
+tinybuddha-wisdom is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+tinybuddha-wisdom is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with tinybuddha-wisdom.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from http.client import HTTPConnection
+
+class TinyBuddhaWisdom:
+    def scrape(self):
+        h1 = HTTPConnection('www.tinybuddha.com')
+        h1.request('GET', '/', 
+        headers={
+            'Accept':'text/html', 
+            'Accept-Charset': 'iso-8859-1', 
+            'Accept-Encoding': '',
+            'Accept-Language': 'en-us,en;q=0.5',
+            'Connection': 'keep-alive',
+            'DNT': '1', 
+            'Host': 'tinybuddha.com',
+            'User-Agent': 'Mozilla/5.0'}
+        )
+
+        response = h1.getresponse()
+        data = str(response.read(), 'utf-8')
+
+        cut = data.split("Click for Quotes")[1]
+        cut = cut.split('home-ad')[0]
+        cut = cut[cut.find('<a'):cut.find('</a')]
+        cut = cut[cut.find('>')+1:]
+        return cut
+
+print(TinyBuddhaWisdom().scrape())
