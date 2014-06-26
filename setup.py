6cc6e9e709ca9cca5ba41237dfc09f5336ec2235
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2014-06-26 11:34:12 +0200

    added classfifiers, updated setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,70 @@
-import ez_setup
-ez_setup.use_setuptools()
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
 
 import os
+import sys
 
 # disables creation of .DS_Store files inside tarballs on Mac OS X
 os.environ['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
 os.environ['COPYFILE_DISABLE'] = 'true'
 
-def autosetup():
-	from setuptools import setup, find_packages
-	return setup(
-		name			= "tinybuddha-wisdom",
-		version			= "1.0",
-		
-		include_package_data = True,
-		zip_safe		= False,
-		packages		= find_packages(),
-		
-		entry_points	= {
-			'console_scripts': [
-				'tinybuddha = tinybuddhawisdom.app:main',
-			]
-		},
-	)
+import tinybuddhawisdom
 
-if(__name__ == '__main__'):
-	dist = autosetup()
+DESCRIPTION = 'Tinybuddha Wisdom is a tiny console fortune-like wisdom teller.'
+
+setup(
+    author=tinybuddhawisdom.__author__,
+    author_email=tinybuddhawisdom.__author_email__,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.3',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Terminals',
+        'Topic :: Utilities',
+    ],
+    data_files=[],
+    description=DESCRIPTION,
+    ext_modules=[],
+    install_requires=[],
+    license=tinybuddhawisdom.__license__,
+    long_description=DESCRIPTION,
+    name='tinybuddhawisdom',
+    packages=find_packages(),
+    package_data={},
+    setup_requires=[],
+    url=tinybuddhawisdom.__url__,
+    use_2to3=(sys.version_info >= (3,)),
+    version=tinybuddhawisdom.__version__,
+    entry_points={'console_scripts' : [
+        'tinybuddha = tinybuddhawisdom.app:main']},
+)
+
